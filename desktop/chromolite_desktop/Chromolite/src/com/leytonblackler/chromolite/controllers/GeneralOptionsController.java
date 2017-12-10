f78ca560146091f7bf1441cbcbb414c9a56baf2c
Leyton Blackler <leytonblackler@gmail.com>
2017-12-10 20:47:52 +1300

    Rearranged the general options menu to include support for future additional options.

--- desktop/chromolite_desktop/Chromolite/src/com/leytonblackler/chromolite/controllers/GeneralOptionsController.java
+++ desktop/chromolite_desktop/Chromolite/src/com/leytonblackler/chromolite/controllers/GeneralOptionsController.java
@@ -3,6 +3,7 @@ package com.leytonblackler.chromolite.controllers;
 import com.leytonblackler.chromolite.main.settings.Settings;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -19,20 +20,34 @@ public class GeneralOptionsController implements Controller, Initializable {
     private ToggleButton loadButton;
 
     @FXML
+    private CheckBox startMinimisedCheckBox;
+
+    @FXML
     private ToggleButton resetButton;
 
     @FXML
     private ToggleButton razerButton;
 
     @FXML
+    private ToggleButton hueButton;
+
+    @FXML
     public ImageView razerLogo;
     private Image[] razerLogoImages = new Image[2];
+
+    @FXML
+    public ImageView hueLogo;
+    private Image[] hueLogoImages = new Image[2];
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         razerLogoImages[0] = new Image(getClass().getClassLoader().getResource("images/razer_logo_light.png").toExternalForm());
         razerLogoImages[1] = new Image(getClass().getClassLoader().getResource("images/razer_logo_dark.png").toExternalForm());
         razerLogo.setImage(razerLogoImages[0]);
+
+        hueLogoImages[0] = new Image(getClass().getClassLoader().getResource("images/hue_logo_light.png").toExternalForm());
+        hueLogoImages[1] = new Image(getClass().getClassLoader().getResource("images/hue_logo_dark.png").toExternalForm());
+        hueLogo.setImage(hueLogoImages[0]);
     }
 
     @Override
@@ -62,6 +77,16 @@ public class GeneralOptionsController implements Controller, Initializable {
             razerLogo.setImage(razerLogoImages[1]);
         } else {
             razerLogo.setImage(razerLogoImages[0]);
+        }
+    }
+
+    @FXML
+    private void hueButtonClicked() {
+        System.out.println("hue");
+        if (hueButton.selectedProperty().get()) {
+            hueLogo.setImage(hueLogoImages[1]);
+        } else {
+            hueLogo.setImage(hueLogoImages[0]);
         }
     }
 }
