39c59d2cb7343a8a7570b47f22cc668b51aec9ba
Riley Blair <rjb@hyper.net.nz>
2018-04-24 00:21:07 +1200

    Added executable boot application

--- web/api/src/main/Application.java
+++ web/api/src/main/Application.java
@@ -1,5 +1,12 @@
 package main;
 
-public class Application {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
