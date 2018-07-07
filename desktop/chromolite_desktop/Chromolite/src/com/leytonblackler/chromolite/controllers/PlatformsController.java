117433385a3eaebc00a81833110482e4aa7ec8f2
Leyton Blackler <leytonblackler@gmail.com>
2018-07-07 17:37:18 +1200

    Initial restructure of the platform section of the UI.

--- desktop/chromolite_desktop/Chromolite/src/com/leytonblackler/chromolite/controllers/PlatformsController.java
+++ desktop/chromolite_desktop/Chromolite/src/com/leytonblackler/chromolite/controllers/PlatformsController.java
@@ -1,58 +1,68 @@
 package com.leytonblackler.chromolite.controllers;
 
-import com.leytonblackler.chromolite.Chromolite;
 import com.leytonblackler.chromolite.main.settings.SettingsManager;
 import com.leytonblackler.chromolite.main.settings.categories.PlatformSettings;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ToggleButton;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.text.TextAlignment;
+import javafx.util.Callback;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PlatformsController  extends Controller implements Initializable {
 
-    @FXML
-    private ToggleButton arduinoButton;
+    private static final String[] PLATFORMS = { "ARDUINO", "RAZER CHROMA", "PHILIPS HUE", "NANOLEAF" };
 
     @FXML
-    private ToggleButton razerButton;
+    private ComboBox platformComboBox;
 
     @FXML
-    private ToggleButton hueButton;
+    private ImageView platformIcon;
 
     @FXML
     private CheckBox syncPlatformsCheckBox;
 
     @FXML
-    public ImageView arduinoLogo;
+    private ToggleButton optionsButton;
+
     private Image[] arduinoLogoImages = new Image[2];
 
-    @FXML
-    public ImageView razerLogo;
     private Image[] razerLogoImages = new Image[2];
 
-    @FXML
-    public ImageView hueLogo;
     private Image[] hueLogoImages = new Image[2];
+
+    private Image[] nanoleafLogoImages = new Image[2];
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         arduinoLogoImages[0] = new Image(getClass().getClassLoader().getResource("images/arduino_logo_light.png").toExternalForm());
         arduinoLogoImages[1] = new Image(getClass().getClassLoader().getResource("images/arduino_logo_dark.png").toExternalForm());
-        arduinoLogo.setImage(arduinoLogoImages[0]);
 
         razerLogoImages[0] = new Image(getClass().getClassLoader().getResource("images/razer_logo_light.png").toExternalForm());
         razerLogoImages[1] = new Image(getClass().getClassLoader().getResource("images/razer_logo_dark.png").toExternalForm());
-        razerLogo.setImage(razerLogoImages[0]);
 
         hueLogoImages[0] = new Image(getClass().getClassLoader().getResource("images/hue_logo_light.png").toExternalForm());
         hueLogoImages[1] = new Image(getClass().getClassLoader().getResource("images/hue_logo_dark.png").toExternalForm());
-        hueLogo.setImage(hueLogoImages[0]);
+
+        nanoleafLogoImages[0] = new Image(getClass().getClassLoader().getResource("images/nanoleaf_logo_light.png").toExternalForm());
+        nanoleafLogoImages[1] = new Image(getClass().getClassLoader().getResource("images/nanoleaf_logo_dark.png").toExternalForm());
+
+        platformIcon.setImage(arduinoLogoImages[0]);
+
+        /*platformComboBox.setItems(FXCollections.observableArrayList(PLATFORMS));
+        platformComboBox.setCellFactory(param -> new PlatformListCell());
+        platformComboBox.setButtonCell(new PlatformListCell());
+        platformComboBox.setValue(PLATFORMS[0]);*/
     }
 
     @FXML
@@ -71,6 +81,16 @@ public class PlatformsController  extends Controller implements Initializable {
     }
 
     @FXML
+    private void platformChoiceBoxChanged() {
+        //
+    }
+
+    @FXML
+    private void optionsButtonClicked() {
+        //
+    }
+
+    @FXML
     private void syncPlatformsCheckBoxClicked() {
         getSettings().setSyncPlatforms(syncPlatformsCheckBox.isSelected());
     }
@@ -85,19 +105,19 @@ public class PlatformsController  extends Controller implements Initializable {
         syncPlatformsCheckBox.setSelected(settings.getSyncPlatforms());
         switch (settings.getPlatform()) {
             case ARDUINO:
-                setButtonSelected(true, arduinoButton, arduinoLogoImages, arduinoLogo);
-                setButtonSelected(false, razerButton, razerLogoImages, razerLogo);
-                setButtonSelected(false, hueButton, hueLogoImages, hueLogo);
+                //setButtonSelected(true, arduinoButton, arduinoLogoImages, arduinoLogo);
+                //setButtonSelected(false, razerButton, razerLogoImages, razerLogo);
+                //setButtonSelected(false, hueButton, hueLogoImages, hueLogo);
                 break;
             case RAZER_CHROMA:
-                setButtonSelected(false, arduinoButton, arduinoLogoImages, arduinoLogo);
-                setButtonSelected(true, razerButton, razerLogoImages, razerLogo);
-                setButtonSelected(false, hueButton, hueLogoImages, hueLogo);
+                //setButtonSelected(false, arduinoButton, arduinoLogoImages, arduinoLogo);
+                //setButtonSelected(true, razerButton, razerLogoImages, razerLogo);
+                //setButtonSelected(false, hueButton, hueLogoImages, hueLogo);
                 break;
             case PHILIPS_HUE:
-                setButtonSelected(false, arduinoButton, arduinoLogoImages, arduinoLogo);
-                setButtonSelected(false, razerButton, razerLogoImages, razerLogo);
-                setButtonSelected(true, hueButton, hueLogoImages, hueLogo);
+                //setButtonSelected(false, arduinoButton, arduinoLogoImages, arduinoLogo);
+                //setButtonSelected(false, razerButton, razerLogoImages, razerLogo);
+                //setButtonSelected(true, hueButton, hueLogoImages, hueLogo);
                 break;
         }
     }
